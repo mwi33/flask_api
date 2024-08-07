ddcb0edcfc270a154a8cbe4724565a347249b4ee
mwi33@protonmail.com <mwi33@protonmail.com>
2024-08-07 11:41:22 +1000

    update url route app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask
 app = Flask(__name__)
 
 # the app.route decorator is used to tell flask what URL should trigger the function
-@app.route('/hello/')
+@app.route('/')
 def hello():
     return "Hello, World"
 
