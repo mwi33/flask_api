7b16e1d33076d1b6ae30c21cb9b80813b43b7a74
mwi33@protonmail.com <mwi33@protonmail.com>
2024-08-07 18:18:37 +1000

    Update new content

--- README.md
+++ README.md
@@ -2,13 +2,18 @@
 Flask api for testing
 Initial README
 
-
+# Include the required libraries with pip3 and the requirements.txt
 ~~~ bash
 # create a requirements.txt for production installation
 pipreqs --ignore virtualenv_flask /home/mwi33/workspaces/flask_api
 
 pip install -r /path/to/requirements.txt
 
+~~~
+
+# Run the application
+
+~~~ bash
 # run flask app
 # in the directory with the app.py file run:
 python app.py
--- app.py
+++ app.py
@@ -1,13 +1,34 @@
 # import flask libraries
-from flask import Flask
+from flask import Flask, render_template
 
 # create an instance of the class
 app = Flask(__name__)
 
+data = [
+    {
+        'name':'Bill',
+        'place':'Perth',
+        'id':'12345'
+    },
+    {
+        'name':'Jane',
+        'place':'Sydney',
+        'id':'6789'
+    },
+    {
+        'name':'James',
+        'place':'Melbourne',
+        'id':'1092'
+    }
+]
+
 # the app.route decorator is used to tell flask what URL should trigger the function
+# this route and function are for when the site index page (/) is accessed with a GET request
 @app.route('/')
 def hello():
-    return "Hello, World"
+    return render_template('index.html', data=data)
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
